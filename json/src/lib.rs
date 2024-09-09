f25bb16c1de2ec39cc4d119862d3df067e8784fd
Reese Russell <admin@qrsnap.io>
2024-09-09 05:33:31 +0000

    add support for string or string array

--- json/src/lib.rs
+++ json/src/lib.rs
@@ -109,6 +109,14 @@ pub struct GetNetworkInfoResultAddress {
     pub score: usize,
 }
 
+/// Used to represent values that can either be a string or a string array.
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
+#[serde(untagged)]
+pub enum StringOrStringArray {
+    String(String),
+    StringArray(Vec<String>),
+}
+
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
 pub struct GetNetworkInfoResult {
     pub version: usize,
@@ -137,7 +145,7 @@ pub struct GetNetworkInfoResult {
     pub incremental_fee: Amount,
     #[serde(rename = "localaddresses")]
     pub local_addresses: Vec<GetNetworkInfoResultAddress>,
-    pub warnings: String,
+    pub warnings: StringOrStringArray,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
